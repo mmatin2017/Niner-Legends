127e252205a6e5f4dbf37ec910769089a067520d
Mohammed Matin <mmatin@uncc.edu>
2021-04-16 17:05:44 -0400

    Summoner data retrieved and displayed

--- niner-legends/src/pages/Home.js
+++ niner-legends/src/pages/Home.js
@@ -11,7 +11,15 @@ import ListGroup from "react-bootstrap/ListGroup";
 export default function Home() {
    const [name, setName] = useState("");
    const [isLoading, setIsLoading] = useState(false);
-   const [state, setState] = useState({data : {}});
+   const [state, setState] = useState({data: {}})
+   const [loading, setLoading] = useState(true);
+   const [id, setId] = useState(0);;
+
+  
+   
+
+  
+
     
     function validateForm() {
       return name.length > 0;
@@ -21,33 +29,47 @@ export default function Home() {
       event.preventDefault();
       setIsLoading(true)
       
-      let id;
+      
         try{
-          
-            await axios.post('http://d0c86f7024dd.ngrok.io/summoners', {name : name})
+            await axios.post('http://41dfa7c801b5.ngrok.io/summoners', {name : name})
             .then(res => {
-              
-              console.log("please work")
-              console.log(res.data)
               for (let j = 0; j < res.data.participantIdentities.length; j++) {
                 console.log(res.data.participantIdentities[j].player.summonerName)
                 if(res.data.participantIdentities[j].player.summonerName === name){
-                  id = j;
+                  setId(j);
                   break;
                 }
               }
-              console.log(res.data.participantIdentities[id].player.summonerName)
-              console.log(res.data.participants[id])
-              setState(res.data.participants[id]);
-              console.log(state.stats.assists)
-              console.log(id);
               
+            })
+            .then()
+            .catch(err => console.error(err))
+            .finally(() => {
+            })
+
+            await axios.get('http://41dfa7c801b5.ngrok.io/participants')
+            .then(res => {
+              console.log(res.data)
+              setState(res.data)
+              console.log(state.champion[id])
+              setLoading(false);
+
+
+              
+             
+              
+              
+              
+              
+
             })
             .catch(err => console.error(err))
             .finally(() => {
                 setIsLoading(false);
-                console.log(state)
+                setLoading(false);
+                
             })
+
 
       }
       catch(event){
@@ -55,37 +77,78 @@ export default function Home() {
         setIsLoading(false);
       }
     }
+      /*
 
+    <ListGroup.Item>Win {win}</ListGroup.Item>
+        <ListGroup.Item>Kills {kills}</ListGroup.Item>
+        <ListGroup.Item>Assists {assists}</ListGroup.Item>
+        <ListGroup.Item>Deaths {deaths}</ListGroup.Item>
+        <ListGroup.Item>Damage {damage}</ListGroup.Item>
+        <ListGroup.Item>Gold {gold}</ListGroup.Item>
+        <ListGroup.Item>Total Minions Kills {minions}</ListGroup.Item>
+        <ListGroup.Item>Item 1 {item1}</ListGroup.Item>
+        <ListGroup.Item>Item 2 {item2}</ListGroup.Item>
+        <ListGroup.Item>Item 3 {item3}</ListGroup.Item>
+        <ListGroup.Item>Item 4 {item4}</ListGroup.Item>
+        <ListGroup.Item>Item 5 {item5}</ListGroup.Item>
+        <ListGroup.Item>Item 6 {item6}</ListGroup.Item>
+        <ListGroup.Item>Item 7 {item7}</ListGroup.Item>
+
+         setKills(res.data.kills[id])              
+              setAssists(res.data.assists[id])
+              setDeaths(res.data.deaths[id])
+              setDamage(res.data.totalDamageDealt[id])
+              setGold(res.data.goldEarned[id])
+              setMinions(res.data.totalMinionsKilled[id])
+              setItem1(res.data.Item1[id])
+              setItem2(res.data.Item2[id])
+              setItem3(res.data.Item3[id])
+              setItem4(res.data.Item4[id])
+              setItem5(res.data.Item5[id])
+              setItem6(res.data.Item6[id])
+              setItem7(res.data.Item7[id])
+    */
       function renderList(){
+        if(loading ===true){
+          return <h4>Enter Data</h4>
+        }
+        else{
       return(
+
         <ListGroup>
         <ListGroup.Item>Summoner {name}</ListGroup.Item>
-        <ListGroup.Item>Champion ID {state.championId}</ListGroup.Item>
-        <ListGroup.Item>Total Assists {state.stats.assists}</ListGroup.Item>
-        <ListGroup.Item>Total Kills {state.stats.kills}</ListGroup.Item>
-
+        <ListGroup.Item>Champion {state.champion[id]}</ListGroup.Item>
+        <ListGroup.Item>Win {state.win[id]}</ListGroup.Item>
+        <ListGroup.Item>Kills {state.kills[id]}</ListGroup.Item>
+        <ListGroup.Item>Deaths {state.deaths[id]}</ListGroup.Item>
+        <ListGroup.Item>Assists {state.assists[id]}</ListGroup.Item>
+        <ListGroup.Item>Total Damage {state.totalDamageDealt[id]}</ListGroup.Item>
+        <ListGroup.Item>Gold Earned {state.goldEarned[id]}</ListGroup.Item>
+        <ListGroup.Item>Champion Level {state.champLevel[id]}</ListGroup.Item>
+        <ListGroup.Item>Minions Killed {state.totalMinionsKilled[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 1 {state.Item1[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 2 {state.Item2[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 3 {state.Item3[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 4 {state.Item4[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 5 {state.Item5[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 6 {state.Item6[id]}</ListGroup.Item>
+        <ListGroup.Item>Item 7 {state.Item7[id]}</ListGroup.Item>
         </ListGroup>
       )
+        }
 
     }
 
 
-    
-    /*useEffect(()=>{
-          
-          axios.post('http://4528c60c4414.ngrok.io/summoners', {name : "Lugerr"})
-         .then(res => setState(res.data))
-         .catch(err => console.error(err))
-        }, [])
-        */
-
   return (
-    
+    <div>
     <div className="lander">
     <h1>Niner Legends</h1>
     <p className="text-muted">
       Competitive stat tracking for League of Legends
     </p>
+    </div>
+    <div className="Login">
     <Form onSubmit={handleSubmit}>
           <Form.Group size="lg" controlId="name">
             <Form.Label>Enter Summoner Name</Form.Label>
@@ -106,12 +169,15 @@ export default function Home() {
             Submit
           </LoaderButton>
         </Form>
+        </div>
 
         {renderList()}
+
+        </div>
 
         
       
         
-  </div>
+  
   );
 }
