d9ec781b8f7ddc57c7485bb5386b777f45a13f7d
Mohammed Matin <mmatin@uncc.edu>
2021-04-16 05:27:30 -0400

    Able to console log api data

--- niner-legends/src/components/Summoner.js
+++ niner-legends/src/components/Summoner.js
@@ -1,76 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import LoaderButton from "../components/LoaderButton";
+import React from "react";
 import "../index.css";
 
-export default function Summoner(){
+export const Summoners = ({summoners}) => {
+
+
+
+  return <div className="Home">{summoners.map(summoner => <div>{summoner.player}</div>)}</div>
+
+};
    
-    const [name, setName] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-  
-    function validateForm() {
-      return name.length > 0;
-    }
-  
-    async function handleSubmit(event) {
-        try{
-            
-            setIsLoading(true);
-            console.log(name);
 
-      }
-      catch(event){
-        console.log(event)
-      }
-    }
-
-  
-
-    useEffect(()=>{
-        try{
-          const summoner = "Son";
-          fetch('http://93ad5ff73620.ngrok.io/summoners', {
-              method: 'POST',
-              headers: {
-                  'Content-Type' : 'application/json'
-              },
-              body: JSON.stringify(summoner)
-          }).then(
-            response => response.json()
-          ).then(data => console.log(data))
-
-          if (Response.ok){
-              console.log("response Worked")
-          }
-      }
-      catch(event){
-        console.log(event)
-      }
-        console.log('component mounted!')
-      },[])
-
-    return (
-        <div className="Home">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group size="lg" controlId="name">
-            <Form.Label>Enter Summoner Name</Form.Label>
-            <Form.Control
-              autoFocus
-              type="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Form.Group>
-          <LoaderButton
-            block
-            size="lg"
-            type="submit"
-            isLoading={isLoading}
-            disabled={!validateForm()}
-          >
-            Submit
-          </LoaderButton>
-        </Form>
-      </div>
-    );
-}
+    
--- niner-legends/src/pages/Home.js
+++ niner-legends/src/pages/Home.js
@@ -1,11 +1,51 @@
-import React from "react";
-import Summoner from "../components/Summoner";
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import LoaderButton from "../components/LoaderButton";
 import "../index.css";
+import axios from 'axios';
+import {Summoners} from '../components/Summoner'
 
 
 
 export default function Home() {
-  
+   const [name, setName] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [state, setState] = useState(null);
+
+    function validateForm() {
+      return name.length > 0;
+    }
+    async function handleSubmit(event) {
+      event.preventDefault();
+      setIsLoading(true)
+        try{
+            
+            await axios.post('http://b63558c6766e.ngrok.io/summoners', {name : name})
+            .then(res => setState(res.data))
+            .catch(err => console.error(err))
+            .finally(() => {
+                setIsLoading(false);
+                console.log(state.participantIdentities);
+                
+                
+            })
+
+      }
+      catch(event){
+        console.log(event)
+        setIsLoading(false);
+      }
+    }
+
+    useEffect(()=>{
+          
+          axios.post('http://b63558c6766e.ngrok.io/summoners', {name : "Lugerr"})
+         .then(res => setState(res.data))
+         .catch(err => console.error(err))
+
+       
+        
+        }, [])
 
   return (
     <div className="lander">
@@ -13,7 +53,27 @@ export default function Home() {
     <p className="text-muted">
       Competitive stat tracking for League of Legends
     </p>
-    <Summoner/>
+    <Form onSubmit={handleSubmit}>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Summoner Name</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <LoaderButton
+            block
+            size="lg"
+            type="submit"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Submit
+          </LoaderButton>
+        </Form>
+        
   </div>
   );
 }
