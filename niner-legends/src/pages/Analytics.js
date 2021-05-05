d75a12233c6e8d71b28d7747b597d51064aaeb6a
Mohammed Matin <mmatin@uncc.edu>
2021-05-05 18:02:38 -0400

    added input fields

--- niner-legends/src/pages/Analytics.js
+++ niner-legends/src/pages/Analytics.js
@@ -1,10 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import LoaderButton from "../components/LoaderButton";
 import "../index.css"
 
 export default function NotFound() {
+
+  const [name, setName] = useState("");
+  const [towerKills, setTowerKills] = useState("");
+  const [firstBlood, setFirstBlood] = useState("");
+  const [inhibitorKills, setInhibitorKills] = useState("");
+  const [baronKills, setBaronKills] = useState("");
+  const [dragonKills, setDragonKills] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return name.length > 0;
+  }
+
+  async function handleSubmit(event){
+
+    event.preventDefault();
+    setIsLoading(true);
+
+    try{
+        console.log(towerKills + " " + name + " " + firstBlood+ " " + inhibitorKills+ " " + dragonKills+ " "+ baronKills)
+        setIsLoading(false);
+    }
+    catch{
+
+    }
+
+    
+
+
+  }
   return (
-    <div className="NotFound text-center">
+    <div className="Login">
       <h3>Analytics Page!</h3>
+
+      <Form onSubmit={handleSubmit}>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Summoner Name</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Tower Kills</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={towerKills}
+              onChange={(e) => setTowerKills(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter First Blood</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={firstBlood}
+              onChange={(e) => setFirstBlood(e.target.value)}
+            />
+            <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Inhibitor Kills</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={inhibitorKills}
+              onChange={(e) => setInhibitorKills(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Baron Kills</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={baronKills}
+              onChange={(e) => setBaronKills(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Enter Dragon Kills</Form.Label>
+            <Form.Control
+              autoFocus
+              type="name"
+              value={dragonKills}
+              onChange={(e) => setDragonKills(e.target.value)}
+            />
+          </Form.Group>
+          </Form.Group>
+          <LoaderButton
+            block
+            size="lg"
+            type="submit"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Submit
+          </LoaderButton>
+        </Form>
     </div>
   );
 }
